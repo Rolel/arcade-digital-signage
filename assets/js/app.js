1c29ca985d74e9fddbd9d11ede1a9e9442788ebb
Rolel <rolel@smile.fr>
2018-10-31 23:27:33 +0100

    [FIX] Cleanup App.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,3 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 require('reveal/index.css');
 require('reveal/theme/default.css');
@@ -20,6 +12,7 @@ Reveal.initialize({
     history: true,
     center: true,
     loop: true,
+    autoSlideStoppable: false,
     width: 1920,
     height: 1080,
     // default/cube/page/concave/zoom/linear/fade/none
