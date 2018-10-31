a06f043912cdce000496591704a2f4d930f1deed
Rolel <rolel@smile.fr>
2018-10-31 11:59:14 +0100

    [FEATURE] Added string representation fo a game

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -45,6 +45,11 @@ class Game
         $this->scoreboard = new ArrayCollection();
     }
 
+    public function __toString(): ?string
+    {
+        return $this->getName();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
